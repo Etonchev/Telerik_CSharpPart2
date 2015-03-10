5a15d3ada1362b70ff204568d05330859465759b
Etonchev <emil.krassimirov@gmail.com>
2015-03-10 12:27:36 +0200

    -

---
+++ 02MultidimentionalArrays/01_FillTheMatrix/FillTheMatrix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class FillTheMatrix
+{
+    public static void printMatrix(int[,] matrix)
+    {
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int column = 0; column < matrix.GetLength(0); column++)
+            {
+                Console.Write("{0,3}", matrix[row, column]);
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int startIndex = 1;
+        //int[,] matrix = new int[n, n];
+        //for (int row = 0; row < n; row++)
+        //{
+        //    for (int column = 0; column < n; column++)
+        //    {
+        //        Console.Write("matrix[{0}, {1}] = ", row, column);
+        //        matrix[row, column] = Convert.ToInt32(Console.ReadLine());
+        //    }
+        //}
+      
+        //Subsection a)
+        //int[,] matrix = new int[n, n];
+        //for (int row = 0; row < n; row++)
+        //{
+        //    for (int column = 0; column < n; column++)
+        //    {
+        //        matrix[column, row] = startIndex;
+        //        startIndex++;
+        //    }
+        //}
+
+        //Subsection b)
+        //int[,] matrix = new int[n, n];
+        //for (int column = 0; column < n; column++)
+        //{
+        //    for (int row = 0; row < n; row++)
+        //    {
+        //        if (column % 2 == 0)
+        //        {
+        //            matrix[row, column] = startIndex;
+        //        }
+        //        else
+        //        {
+        //            matrix[n - row - 1, column] = startIndex;
+        //        }
+        //        startIndex++;
+        //    }
+        //}
+
+        //Subsection c)
+
+        printMatrix(matrix);
+    }
+}
---
+++ 02MultidimentionalArrays/02_MaximalSum/MaximalSum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class MaximalSum
+{
+    static void Main()
+    {
+        int row = 0;
+        int col = 0;
+        Console.Write("Enter number of rows: ");
+        row = int.Parse(Console.ReadLine());
+        Console.Write("Enter number of colons: ");
+        col = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[row, col];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                Console.Write("matrix[{0}, {1}] = ", i, j);
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        int currentSum = 0;
+        int maxSum = int.MinValue;
+
+        for (int i = 0; i <= row - 3; i++)
+        {
+            for (int j = 0; j <= col - 3; j++)
+            {
+                currentSum = squareSum(matrix, i, j);
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                }
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("The maximum sum is: {0}", maxSum);
+    }
+
+    static int squareSum(int[,] matrix, int rowIndex, int colIndex)
+    {
+        int result = 0;
+
+        for (int i = rowIndex; i < rowIndex + 3; i++)
+        {
+            for (int j = colIndex; j < colIndex + 3; j++)
+            {
+                result += matrix[i, j];
+            }
+        }
+
+        return result;
+    }
+}
